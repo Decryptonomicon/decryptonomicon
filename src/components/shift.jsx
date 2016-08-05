3749a7a1e850f8c2d384e1fc6422aca40f7f179f
defektive <sirbradleyd@gmail.com>
2016-08-05 15:42:17 -0600

    remove shifter to its own npm package

--- src/components/shift.jsx
+++ src/components/shift.jsx
@@ -1,93 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 
-var Shifter = (function (){
-  // ABC's
-  var charset = [];
-  for (var i = 65; i <= 90; i++) {
-    charset.push(String.fromCharCode(i));
-  }
-
-  function Shifter (key) {
-    this.setKey(key);
-  }
-
-  Shifter.prototype.setKey = function (key) {
-    key = key || "";
-    this._key = key.toUpperCase().replace(/[^A-Z]/g, '');
-  };
-
-  Shifter.prototype.encrypt = function (clearText) {
-    var ctArray = clearText.toUpperCase().replace(/[^A-Z]/g, '').split(''),
-        ctArrayLen = ctArray.length,
-        cipherText = [],
-        keyLength = this._key.length;
-
-    for (var i = 0; i < ctArrayLen; i++) {
-      var clearTextCharIndex = charset.indexOf(ctArray[i]) ,
-          keyIndex = i % keyLength,
-          keyCharIndex = charset.indexOf(this._key[keyIndex]) ;
-
-      // +1 since we are doing zero based character values
-      cipherText.push(charset[(clearTextCharIndex + keyCharIndex + 1) % charset.length]);
-    }
-
-    return normalize(cipherText, clearText);
-  };
-
-  Shifter.prototype.decrypt = function (cipherText) {
-
-      var ctArray = cipherText.toUpperCase().replace(/[^A-Z]/g, '').split(''),
-          ctArrayLen = ctArray.length,
-          clearText = [],
-          keyLength = this._key.length;
-
-      for (var i = 0; i < ctArrayLen; i++) {
-        var cipherTextCharIndex = charset.indexOf(ctArray[i]) ,
-            keyIndex = i % keyLength,
-            keyCharIndex = charset.indexOf(this._key[keyIndex]) ;
-
-        // +1 since we are doing zero based character values
-        clearText.push(charset[(charset.length + (cipherTextCharIndex - (keyCharIndex + 1))) % charset.length]);
-      }
-
-      return normalize(clearText, cipherText);
-  };
-
-  function normalize (input, outputFormat) {
-    var output = [],
-        outputFormat = outputFormat.split(""),
-        ofLen = outputFormat.length,
-        ii = 0;
-
-    for(var i = 0; i < ofLen; i++) {
-      var ofChar = outputFormat[i],
-          ofUpper = ofChar.toUpperCase(),
-          validChar = charset.indexOf(ofUpper) > -1,
-          isUpperCase = ofUpper === ofChar;
-
-      if (!validChar) {
-        output.push(ofChar);
-        continue;
-      }
-
-      if(!isUpperCase) {
-        output.push(input[ii].toLowerCase());
-      } else {
-        output.push(input[ii]);
-      }
-      ii++;
-    }
-
-    return output.join("")
-  }
-
-
-
-  return Shifter;
-})();
-
-
+import Shifter from 'shifter';
 var myShift = new Shifter();
+
 const Shift = React.createClass({
   getInitialState() {
     return {
@@ -119,7 +34,6 @@ const Shift = React.createClass({
   },
 
   update() {
-    console.log(this.state)
     this.clearText.value = myShift[this.state.mode](this.cipherText.value);
   },
 
@@ -187,8 +101,5 @@ const Shift = React.createClass({
     );
   }
 });
-
-
-
 
 export default Shift;
